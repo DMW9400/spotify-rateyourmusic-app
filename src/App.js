972bf24397391a6b73c552e4cdda4d7e62a2314c
github username <github email address>
2018-08-30 15:24:43 -0400

    starting work on fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Homepage from './Components/Homepage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Homepage></Homepage>
       </div>
     );
   }
--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 
 class Homepage extends Component {
 
-  render(
-    return(){
-      <div>
-        HELLO
-      </div>
-    }
-  )
+  handleSubmit = (event) => {
 
+    event.preventDefault()
+
+    let accessToken = '600a5bd2e1554c0ab6c8fd7ae37b27d9'
+
+    return fetch('https://api.spotify.com/v1/search', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + `${accessToken}`,
+          q: 'Pink%20Floyd',
+          type: 'artist'
+      }
+
+      }).then(res => res.json())
+    .then (res => console.log(res));
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>HEAD</h1>
+        <form onSubmit={this.handleSubmit} label='Submit'>
+          <button>
+            Button
+          </button>
+        </form>
+      </div>
+      )
+  }
 }
+
+export default Homepage
